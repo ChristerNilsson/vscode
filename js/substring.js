eddd5631781a104f0c1f71c94dc82bba141ec503
ChristerNilsson <janchrister.nilsson@gmail.com>
2019-05-17 17:52:48 +0200

    'x'

--- js/substring.js
+++ js/substring.js
@@ -3,7 +3,7 @@ var assert, print;
 
 assert = function(flag) {
   if (!flag) {
-    throw 'assert error!';
+    return raise('assert error!');
   }
 };
 
@@ -11,5 +11,5 @@ print = console.log;
 
 assert(true);
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3Vic3RyaW5nLmpzIiwic291cmNlUm9vdCI6Ii4uIiwic291cmNlcyI6WyJzdWJzdHJpbmcuY29mZmVlIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxJQUFBLE1BQUEsRUFBQTs7QUFBQSxNQUFBLEdBQVMsUUFBQSxDQUFDLElBQUQsQ0FBQTtFQUFVLElBQUcsQ0FBSSxJQUFQO0lBQWlCLE1BQU0sZ0JBQXZCOztBQUFWOztBQUNULEtBQUEsR0FBUSxPQUFPLENBQUM7O0FBRWhCLE1BQUEsQ0FBTyxJQUFQIiwic291cmNlc0NvbnRlbnQiOlsiYXNzZXJ0ID0gKGZsYWcpIC0+IGlmIG5vdCBmbGFnIHRoZW4gdGhyb3cgJ2Fzc2VydCBlcnJvciEnXHJcbnByaW50ID0gY29uc29sZS5sb2cgXHJcblxyXG5hc3NlcnQgdHJ1ZSAiXX0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3Vic3RyaW5nLmpzIiwic291cmNlUm9vdCI6Ii4uIiwic291cmNlcyI6WyJzdWJzdHJpbmcuY29mZmVlIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7QUFBQSxJQUFBLE1BQUEsRUFBQTs7QUFBQSxNQUFBLEdBQVMsUUFBQSxDQUFDLElBQUQsQ0FBQTtFQUFVLElBQUcsQ0FBSSxJQUFQO1dBQWlCLEtBQUEsQ0FBTSxlQUFOLEVBQWpCOztBQUFWOztBQUNULEtBQUEsR0FBUSxPQUFPLENBQUM7O0FBRWhCLE1BQUEsQ0FBTyxJQUFQIiwic291cmNlc0NvbnRlbnQiOlsiYXNzZXJ0ID0gKGZsYWcpIC0+IGlmIG5vdCBmbGFnIHRoZW4gcmFpc2UgJ2Fzc2VydCBlcnJvciEnXHJcbnByaW50ID0gY29uc29sZS5sb2cgXHJcblxyXG5hc3NlcnQgdHJ1ZSAiXX0=
 //# sourceURL=c:\github\vscode\substring.coffee
--- substring.coffee
+++ substring.coffee
@@ -1,4 +1,4 @@
-assert = (flag) -> if not flag then throw 'assert error!'
+assert = (flag) -> if not flag then raise 'assert error!'
 print = console.log 
 
 assert true 
